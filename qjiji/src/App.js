514718941230e4402cbbc7f429edf5dd5e4beb6f
geotrieu <geocrafterserver@gmail.com>
2019-11-26 02:32:09 -0500

    Added file

---
+++ qjiji/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navigation from './Navigation';
+import Home from './Home';
+
+function App() {
+  return (
+    <div className="App">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+      <Navigation />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
